86a0b31ac2173658b2043a03f5dbf25b0b237014
Rhuan Guilherme <rhuan23032004@gmail.com>
2025-11-26 23:12:49 -0300

    :card_file_box: Configuração de client do prisma ORM para comunicação com o banco de dados

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,10 @@
 import fastify from 'fastify';
+import { prisma } from './lib/prisma';
 
 export const app = fastify();
+
+app.get('/', async (req, res) => {
+  const user = await prisma.user.findFirst();
+
+  return res.send({ user });
+});
